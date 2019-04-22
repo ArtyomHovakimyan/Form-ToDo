77a9380dcafb3817e37a96d086b86bfa0a81cdee
User <artyom.hovakimyan01@gmail.com>
2019-04-22 17:55:48 +0400

    add datagridview

---
+++ Mic.Volo.FormToDo/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Mic.Volo.FormToDo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataDataGridView1 = new System.Windows.Forms.DataGridView();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataDataGridView1
+            // 
+            this.dataDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataDataGridView1.Location = new System.Drawing.Point(22, 93);
+            this.dataDataGridView1.MultiSelect = false;
+            this.dataDataGridView1.Name = "dataDataGridView1";
+            this.dataDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataDataGridView1.Size = new System.Drawing.Size(754, 275);
+            this.dataDataGridView1.TabIndex = 0;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(701, 64);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "UPDATE";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(485, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(620, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "EDIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(701, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.dataDataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataDataGridView1;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Mic.Volo.FormToDo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Mic.Volo.FormToDo
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = db_cs1; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionString);
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommand command = new SqlCommand();
+        public DataSet ds = new DataSet();
+        string currentId = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetRecords();
+
+        }
+        public void GetRecords()
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("Select * from tbl_tasks", connectionString);
+            adapter.Fill(ds, "tbl_tasks");
+            dataDataGridView1.DataSource = ds;
+            dataDataGridView1.DataMember = "tbl_tasks";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("INSERT INTO tbl_tasks (Task_name) values ('"+textBox1.Text+"')",connection);
+            adapter.Fill(ds, "tbl_tasks");
+            textBox1.Clear();
+            GetRecords();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = dataDataGridView1.CurrentRow.Index;
+            currentId = dataDataGridView1[0, i].Value.ToString();
+            textBox1.Text = dataDataGridView1[1, i].Value.ToString();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("update tbl_tasks set Task_name ='"+textBox1.Text+"' where Id= "+currentId,connection);
+            adapter.Fill(ds, "tbl_tasks");
+            textBox1.Clear();
+            GetRecords();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataDataGridView1.CurrentRow.Index;
+            currentId = dataDataGridView1[0, i].Value.ToString();
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("delete from tbl_tasks where id="+currentId,connection);
+            adapter.Fill(ds, "tbl_tasks");
+            textBox1.Clear();
+            GetRecords();
+        }
+    }
+}
